207d302840c0eafb6375bddeff61229f690ebbdf
Ryan Spilhaus <rspilhaus@mbbagency.com>
2017-01-20 15:34:51 -0600

    Site Commit
    This is the major commit from local to prod

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# space
-fun spaceship project
+# Space
+A fun little spaceship project inspired by [https://dribbble.com/shots/1323389-Meteor](https://dribbble.com/shots/1323389-Meteor) and animated with [GSAP](https://greensock.com/gsap).
---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html class="no-js">
+	<head>
+		
+		
+		<!-- Meta Info -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">		
+		<meta name="description" content="A fun little side project by Ryan Spilhaus">
+		<meta name="og:title" content="Space">
+		<meta name="og:description" content="A fun little side project by Ryan Spilhaus">
+		<meta name="og:image" content="build/images/facebook.jpg">
+		
+		<title>Space | A RyanSpilhaus.com Side Project</title>
+		
+		
+		<!--Favicons-->
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/manifest.json">
+		<meta name="theme-color" content="#ffffff">
+		
+		
+		
+		<!-- CSS -->		
+		<link rel="stylesheet" type="text/css" href="build/css/style.css">
+		
+	</head>
+<body>
+
+<div class="starfield">
+	<div class="star" id="star-1"></div>
+	<div class="star" id="star-2"></div>
+	<div class="star" id="star-3"></div>
+	<div class="star" id="star-4"></div>
+	<div class="star" id="star-5"></div>
+	<div class="star" id="star-6"></div>
+	<div class="star" id="star-7"></div>
+	<div class="star" id="star-8"></div>
+	<div class="star" id="star-9"></div>
+	<div class="star" id="star-10"></div>
+	<div class="star" id="star-11"></div>
+	<div class="star" id="star-12"></div>
+	<div class="star" id="star-13"></div>
+	<div class="star" id="star-14"></div>
+	<div class="star" id="star-15"></div>
+	<div class="star" id="star-16"></div>
+	<div class="star" id="star-17"></div>
+	<div class="star" id="star-18"></div>
+	<div class="star" id="star-19"></div>
+	<div class="star" id="star-20"></div>
+</div><!--end .starfield-->
+
+<div class="planet" id="planet-1"></div>
+<div class="planet" id="planet-2"></div>
+<div class="planet" id="planet-3"></div>
+<div class="planet" id="planet-4"></div>
+<div class="planet" id="planet-5"></div>
+
+<div class="rocket">
+	<?php echo file_get_contents("build/svg/rocket.svg"); ?>
+</div><!--end .rocket-->
+
+
+
+
+
+<!-- Javascript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
+<script src="build/js/production.min.js"></script>	
+</body>	
+</html>
+	
+	
+	
